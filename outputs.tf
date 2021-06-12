f0c9a1f1db794baa2522ff21c45feb88a3867b3b
DRY411S <DRY411S@users.noreply.github.com>
2021-06-12 17:11:23 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,7 @@ output "URI" {
   value       = google_compute_network.vpc_network.self_link
   description = "The self_link URI"
 }
+output "default_deleted" {
+  value       = google_compute_network.vpc_network.delete_default_routes_on_create
+  description = "Are the default routes deleted?"
+}
