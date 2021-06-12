172894119344d3058f9f274ab064d2fed0c623a3
DRY411S <DRY411S@users.noreply.github.com>
2021-06-12 16:07:58 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,32 +1,9 @@
-/*
-  provider "aws" {
-  region = var.aws_region
-}
-
-provider "random" {}
-
-resource "random_pet" "table_name" {}
-
-resource "aws_dynamodb_table" "tfc_example_table" {
-  name = "${var.db_table_name}-${random_pet.table_name.id}"
-
-  read_capacity  = var.db_read_capacity
-  write_capacity = var.db_write_capacity
-  hash_key       = "UUID"
-
-  attribute {
-    name = "UUID"
-    type = "S"
-  }
-}
-*/
-  
 provider "google" {
   credentials = var.service_key
 
   project = var.project_id
-  region  = "us-central1"
-  zone    = "us-central1-c"
+  region  = var.region
+  zone    = var.zone"us-central1-c"
 }
 
 resource "google_compute_network" "vpc_network" {
