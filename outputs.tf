e926fe196532e140f8359da18d537d2c62a3b68c
DRY411S <DRY411S@users.noreply.github.com>
2021-06-12 16:52:42 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,7 @@ output "vpc_id" {
   description = "ID of project VPC"
   value       = google_compute_network.vpc_network.id
 }
-output "vpc_config" {
-  description = "The routing configuration"
-  value       = google_compute_network.vpc_network.routingConfig
+output "vpc" {
+  value       = google_compute_network.vpc_network.vpc
+  description = "The network info"
 }
