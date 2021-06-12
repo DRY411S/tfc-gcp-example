e7d1d7addbff9fe90b4249a53b2caf4760dfac97
DRY411S <DRY411S@users.noreply.github.com>
2021-06-12 16:10:05 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,28 +1,14 @@
-/*
-  variable "aws_region" {
-  type    = string
-  default = "us-west-1"
-}
-
-variable "db_table_name" {
-  type    = string
-  default = "terraform-learn"
-}
-
-variable "db_read_capacity" {
-  type    = number
-  default = 1
-}
-
-variable "db_write_capacity" {
-  type    = number
-  default = 1
-}
-*/
   variable "service_key" {
   type    = string
 }
   variable "project_id" {
   type    = string
 }
-
+  variable "region" {
+  type    = string
+  default = "us-central1"
+}
+  variable "zone" {
+  type    = string
+  default = "us-central1-c"
+}
