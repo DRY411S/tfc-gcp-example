f45fa95dbe9714461ee42f94edee7bd9f8084515
DRY411S <DRY411S@users.noreply.github.com>
2021-06-12 16:59:07 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,7 @@ output "vpc_id" {
   description = "ID of project VPC"
   value       = google_compute_network.vpc_network.id
 }
-output "vpc" {
-  value       = google_compute_network.vpc
-  description = "The network info"
+output "Gateway IP" {
+  value       = google_compute_network.vpc_network.gateway_ipv4
+  description = "The VPC network Gateway IP"
 }
